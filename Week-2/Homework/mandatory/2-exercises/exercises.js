b0fd3bbaaa0332ef15325a14769767f3c3f61a05
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-03-01 21:59:18 +0200

    exerciseTwo

--- Week-2/Homework/mandatory/2-exercises/exercises.js
+++ Week-2/Homework/mandatory/2-exercises/exercises.js
@@ -41,7 +41,24 @@ function exerciseOne(arrayOfPeople) {
  */
 function exerciseTwo(shopping) {
   //Write your code in here
- 
+  const findElement = document.getElementById('content'); // finding my target element.
+
+  let unorderedList = document.createElement('ul');
+  unorderedList.innerText = 'Shopping List';
+
+  shopping.forEach(shoppingItem => {
+    // console.log(shoppingItem);          // what does shopping item access?
+    //  create a list item for each item in the 'shopping' array
+    let listItem = document.createElement('li');
+    listItem.innerText = shoppingItem
+  
+    unorderedList.appendChild(listItem);
+  
+    findElement.appendChild(unorderedList);
+  })
+
+  
+
 }
 
 /**
